4fe21b57b808f75f46832c7b59bf1351f61b38ac
renan <renan@aestesis.org>
2017-10-19 13:22:44 +0200

    added Cfreetype

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/aestesis/Cpng", from:"2.0.2"),
         .package(url: "https://github.com/aestesis/CPango.git", .branch("master")),
+        .package(url: "https://github.com/aestesis/Cfreetype.git", .branch("master")),
         .package(url: "https://github.com/aestesis/libtess.git", from:"1.0.4"),
         .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", from:"17.0.0"),
         .package(url: "https://github.com/aestesis/Cairo.git", .branch("master")),
